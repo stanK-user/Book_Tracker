96fd9ff651567c563b3dbe53893f6aa472c3a464
stanK-user <ginsoakedboyprod@proton.me>
2025-07-31 13:23:26 -0400

    Tons of fixes. All 1-7 options are functional

--- storage.py
+++ storage.py
@@ -1,27 +1,17 @@
 import json
 from pathlib import Path
+from config import FILE_PATH
 
-
-# Define where to store the books data
-BOOKS_FILE = Path("data/books.json")
 
 # Load the current books from the file, or create empty structure if the file doesn't exist
-def load_books(file_path):
-    if not BOOKS_FILE.exists() or BOOKS_FILE.stat().st_size == 0:
+def load_books():
+    if not FILE_PATH.exists() or FILE_PATH.stat().st_size == 0:
         return {"to_read": [], "completed": []}
-    with open(BOOKS_FILE, "r") as file:
+    with open(FILE_PATH, "r") as file:
         return json.load(file)
         
-    
 # Save the current books data to the JSON file
-def save_books(file_path, books):
-    BOOKS_FILE.parent.mkdir(parents=True, exist_ok=True)    # Makes sure 'data/' exists
-    with open(BOOKS_FILE, "w") as file:
+def save_books(books):
+    FILE_PATH.parent.mkdir(parents=True, exist_ok=True)    # Makes sure 'data/' exists
+    with open(FILE_PATH, "w") as file:
         json.dump(books, file, indent=4)
-
-
-# Example of how to use it 
-if __name__ == "__main__":
-    books = load_books()
-    print("Books loaded successfully.")
-    print(books)
