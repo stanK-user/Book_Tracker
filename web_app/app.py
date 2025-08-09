c6dd617e503e9f083821de633774c6bbd885e5da
stanK-user <ginsoakedboyprod@proton.me>
2025-08-09 09:37:48 -0400

    UI Fixes and Sorting Added

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from books import add_book, mark_book_completed, remove_book, get_all_books
+from datetime import datetime
+import os
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     filter_by = request.args.get("filter")  # can be "completed", "to_read", or None
+    sort_type = request.args.get('sort', '')
     books = get_all_books()
 
     if filter_by == "completed":
@@ -13,7 +18,24 @@ def index():
     elif filter_by == "to_read":
         books = [book for book in books if not book.get("completed")]
 
-    return render_template("index.html", books=books, filter=filter_by)
+    if sort_type == 'title_asc':
+         books.sort(key=lambda b: b['title'].lower())
+    elif sort_type == 'title_desc':
+        books.sort(key=lambda b: b['title'].lower(), reverse=True)
+    elif sort_type == 'author_asc':
+        books.sort(key=lambda b: b['author'].lower())
+    elif sort_type == 'author_desc':
+        books.sort(key=lambda b: b['author'].lower(), reverse=True)
+    elif sort_type == 'newest':
+        books.sort(key=lambda b: datetime.fromisoformat(b['date_added']), reverse=True)
+    elif sort_type == 'oldest':
+        books.sort(key=lambda b: datetime.fromisoformat(b['date_added']))
+    elif sort_type == 'completed_first':
+        books.sort(key=lambda b: (not b['completed'], b['title'].lower()))
+    elif sort_type == 'to_read_first':
+        books.sort(key=lambda b: (b['completed'], b['title'].lower()))
+
+    return render_template("index.html", books=books, filter=filter_by, sort=sort_type)
 
 @app.route("/add", methods=["POST"])
 def add():
